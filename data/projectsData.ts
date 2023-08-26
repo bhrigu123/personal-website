4f4d23447a821e563aa5e4b0b47c70b1c7eae2c6
Bhrigu <captain.bhrigu@gmail.com>
2023-08-26 16:39:29 -0700

    projects page. NextJS blog post. Increased font size in posts

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,20 +1,28 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'JEEQuery',
+    description: `A stackoverflow like Q&A Application for engineering entrance examinations in India. 2015-2018`,
+    imgSrc: '/static/images/jq/profile_500.png',
+    href: '/blog/deployed-and-hosted',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Instant Lyrics',
+    description: `Back in the days there wasn't a lyrics feature on Spotify. I made an app to show the lyrics of the currently playing Spotify song`,
+    imgSrc: '/static/images/lyrics/lyrics.png',
+    href: 'https://github.com/bhrigu123/Instant-Lyrics',
   },
+  {
+    title: 'DTU Mart',
+    description: `An online portal for my college students to buy, sell and exchanges books and other study materials. 2016-2018`,
+    imgSrc: '/static/images/projects/dm_home.png',
+    href: 'https://www.youtube.com/watch?v=iTk_-WJ_Z8M&ab_channel=BhriguSrivastava'
+  },
+  {
+    title: 'Classifier',
+    description: `A simple Python script to organize files in your directories`,
+    imgSrc: '/static/images/projects/classifier.png',
+    href: 'https://github.com/bhrigu123/classifier'
+  }
 ]
 
 export default projectsData
