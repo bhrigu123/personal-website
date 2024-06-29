376245f5fe239dd5cfd5cd721d39b7e8239d2f88
Bhrigu Srivastava <captain.bhrigu@gmail.com>
2024-06-28 19:25:02 -0700

    other updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,7 +25,7 @@ module.exports = {
         sans: ['Outfit', ...fontFamily.sans],
       },
       colors: {
-        primary: colors.violet,
+        primary: colors.teal,
         gray: colors.gray,
         
       },
