58f75b05e09744b0a077135a40ae265c90d8e738
Bhrigu Srivastava <captain.bhrigu@gmail.com>
2024-07-05 23:18:13 -0700

    header updates

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,6 +9,7 @@ import ThemeSwitch from './ThemeSwitch'
 import SearchButton from './SearchButton'
 import { usePathname } from 'next/navigation'
 import Image from 'next/image'
+import avatar from '../public/static/images/avatar2.jpeg'
 
 const Header = () => {
   const pathname = usePathname();
@@ -17,25 +18,32 @@ const Header = () => {
       {pathname === '/'
         ? <div className='flex flex-1'></div>
         : 
-        <div className='flex flex-1'><Link
+        <div className='flex flex-1'>
+          <Link
           href={'/'}>
-          Home
+          <Image
+                src={avatar}
+                alt="avatar"
+                width={512}
+                height={512}
+                className="h-8 w-8 rounded-full"
+              />
         </Link></div>
       }
 
-      <div className="flex flex-1 justify-center leading-5 space-x-4 sm:space-x-6">
-        {headerNavLinks
-          .filter((link) => link.href !== '/')
-          .map((link) => (
-            <Link
-              key={link.title}
-              href={link.href}
-              className="hidden sm:block font-medium text-gray-900 dark:text-gray-100"
-            >
-              {link.title}
-            </Link>
-          ))}
-
+        <div className='hidden sm:flex flex-1 justify-center items-center leading-5 space-x-4 sm:space-x-6 rounded-full bg-white/90 px-3 font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10'>
+          {headerNavLinks
+            .filter((link) => link.href !== '/')
+            .map((link) => (
+              <Link
+                key={link.title}
+                href={link.href}
+                className="hidden sm:block font-medium text-gray-900 dark:text-gray-100"
+              >
+                {link.title}
+              </Link>
+            ))}
+        
       </div>
       <div className='flex md:flex-1 justify-end leading-5 space-x-4 sm:space-x-6'>
         <SearchButton />
