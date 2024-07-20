78d7e66797bf3dde3ba6cc8c882dbc1df9fb5770
Bhrigu Srivastava <captain.bhrigu@gmail.com>
2024-07-20 16:50:25 -0700

    redirect to bhrigu.dev

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
 import { sortPosts, allCoreContent } from 'pliny/utils/contentlayer'
 import { allBlogs } from 'contentlayer/generated'
 import Main from './Main'
+import { permanentRedirect } from 'next/navigation'
+import { headers } from 'next/headers'
 
 export default async function Page() {
+  const header = headers()
+  if (header.get('host') === 'bhrigu.me') {
+    permanentRedirect('https://bhrigu.dev')
+  }
+
   const sortedPosts = sortPosts(allBlogs)
   const posts = allCoreContent(sortedPosts)
   return <Main posts={posts} />
